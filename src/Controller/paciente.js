a2c3ffbc79fbeda8583058ddbf2d925b72258509
Pedro Ferri <pedroferri@edu.unifil.br>
2023-10-31 16:51:49 -0300

    Criancao do Arquivo Verifica.js Criacao do arquivo Verifica para colocar as funcoes de verificacoes dentro dele e chamalas sem 'poluir' o codigo

--- src/Controller/paciente.js
+++ src/Controller/paciente.js
@@ -1,24 +1,9 @@
 import { DatabaseSQL } from "../DB/paciente.js";
-import { connection } from "../DB/db.js";
+
+import { checkHospitalExists } from "../config/Verifica.js"
 const database = new DatabaseSQL();
 
 export default async function (fastify) {
-	async function checkHospitalExists(nomeHospital) {
-		// Consulta para verificar a existência do hospital pelo nome
-		// console.log('Verificando hospital:', nomeHospital);
-		const result = await connection.query(
-			"SELECT ID_Hospital FROM hospital WHERE Nome_Hospital = ?",
-			[nomeHospital]
-		);
-		// console.log('Resultado da consulta:', result);
-		// Verifica se o array tem pelo menos 1 elemento, depois verifica se o primeiro elemento tem pelo menos 1 elementro dentro dele
-		if (result && result.length > 0 && result[0].length > 0) {
-			const idHospital = result[0][0].ID_Hospital;
-			// console.log('ID do Hospital:', idHospital);
-			return idHospital;
-		}
-		return null;
-	}
 
 	fastify.post("/paciente", async (request, reply) => {
 		const { name, adress, alergia, nomeHospital } = request.body;
